2170e624ba631663922bdff1c3f475f1963833ca
ugra92 <ugra1992@gmail.com>
2015-08-02 23:20:32 +0200

    optimize yt

--- kms/src/AppBundle/Entity/Article.php
+++ kms/src/AppBundle/Entity/Article.php
@@ -13,7 +13,7 @@ use Doctrine\ORM\Mapping as ORM;
  *
  */
 
-class Article {
+class Article{
     /**
      * @ORM\Id
      * @ORM\Column(type="integer")
--- kms/src/AppBundle/Entity/Post.php
+++ kms/src/AppBundle/Entity/Post.php
@@ -7,84 +7,11 @@
  */
 
 namespace AppBundle\Entity;
-use Doctrine\ORM\Mapping as ORM;
-use Doctrine\ORM\Mapping\JoinColumn;
-use Doctrine\ORM\Mapping\OneToOne;
 
-/**
- * @ORM\Entity()
- *
- */
+
+
+
 class Post {
-
-    /**
-     * @ORM\Id
-     * @ORM\Column(type="integer")
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    protected $postId;
-
-    /**
-     * @OneToOne(targetEntity="AppBundle\Entity\Article")
-     * @JoinColumn(name="article_id", referencedColumnName="articleId", nullable=true)
-     **/
-    protected $articleId;
-
-    /**
-     * @OneToOne(targetEntity="AppBundle\Entity\Video")
-     * @JoinColumn(name="video_id", referencedColumnName="videoId", nullable=true)
-     **/
-    protected $videoId;
-
-    /**
-     * @return mixed
-     */
-    public function getPostId()
-    {
-        return $this->postId;
-    }
-
-    /**
-     * @param mixed $postId
-     */
-    public function setPostId($postId)
-    {
-        $this->postId = $postId;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getArticleId()
-    {
-        return $this->articleId;
-    }
-
-    /**
-     * @param mixed $articleId
-     */
-    public function setArticleId($articleId)
-    {
-        $this->articleId = $articleId;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getVideoId()
-    {
-        return $this->videoId;
-    }
-
-    /**
-     * @param mixed $videoId
-     */
-    public function setVideoId($videoId)
-    {
-        $this->videoId = $videoId;
-    }
-
-
-
+    private $postId;
 
 }
